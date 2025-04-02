eecd1055909723e2f1f04ca46ab1d4e20f04c1ef
JørR <43743804+Ezyrone@users.noreply.github.com>
2025-04-02 15:56:13 +0200

    feat(Includes): Add Maintenance factory include file

---
+++ include/patterns/MaintenanceFactory.hpp
@@ -0,0 +1,14 @@
+#ifndef MAINTENANCE_FACTORY_HPP
+#define MAINTENANCE_FACTORY_HPP
+
+#include "InterventionFactory.hpp"
+#include "../interventions/Maintenance.hpp"
+
+class MaintenanceFactory : public InterventionFactory {
+public:
+    Intervention* createIntervention() const override {
+        return new Maintenance();
+    }
+};
+
+#endif

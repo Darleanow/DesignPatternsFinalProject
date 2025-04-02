2e20ee743604c0d270ab5a745fb8078b9dd5171c
JørR <43743804+Ezyrone@users.noreply.github.com>
2025-04-02 16:05:37 +0200

    test(Main): Add test code in main for factory

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include "../patterns/MaintenanceFactory.hpp"
+#include "../patterns/UrgenceFactory.hpp"
 
 int main() {
-    std::cout << "Bienvenue dans le système de planification d'interventions !" << std::endl;
+    std::cout << "--- Planification d'interventions ---" << std::endl;
+
+    MaintenanceFacotory maintenanceFactory;
+    UrgenceFactory urgenceFactory;
+
+    Intervention* intervention1 = maintenanceFactory.createIntervention();
+    Intervention* intervention2 = urgenceFactory.createIntervention();
+
+    std::cout << "Intervention 1: " << intervention1->getType() << " - " << intervention1->getDetails() << std::endl;
+    std::cout << "Intervention 2: " << intervention2->getType() << " - " << intervention2->getDetails() << std::endl;
+
+    delete intervention1;
+    delete intervention2;
+
     return 0;
 }

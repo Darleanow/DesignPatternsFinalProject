1449a473cc89b7a8553b69915212ca9433c96dc6
JørR <43743804+Ezyrone@users.noreply.github.com>
2025-04-02 15:58:31 +0200

    feat(Includes): Add urgence factory include file

---
+++ include/patterns/UrgenceFactory.hpp
@@ -0,0 +1,14 @@
+#ifndef URGENCE_FACTORY_HPP
+#define URGENCE_FACTORY_HPP
+
+#include "InterventionFactory.hpp"
+#include "../interventions/Urgence.hpp"
+
+class UrgenceFactory : public InterventionFactory {
+public:
+    Intervention* createIntervention() const override {
+        return new Urgence();
+    }
+};
+
+#endif

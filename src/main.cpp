b6cedc994075b6a696632a7796066db9ac7c7bd3
JørR <43743804+Ezyrone@users.noreply.github.com>
2025-04-02 18:22:06 +0200

    test(Main): Add code to test decorator factory

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
+
+// Factory
 #include "../include/patterns/MaintenanceFactory.hpp"
 #include "../include/patterns/UrgenceFactory.hpp"
 
+// Decorators
+#include "../include/patterns/SuiviGPSDecorator.hpp"
+#include "../include/patterns/PiecesJointesDecorator.hpp"
+
 int main() {
-    std::cout << "--- Planification d'interventions ---" << std::endl;
+    std::cout << "=== Bienvenue dans le système de planification d'interventions ===\n" << std::endl;
 
     MaintenanceFactory maintenanceFactory;
     UrgenceFactory urgenceFactory;
@@ -11,11 +17,31 @@ int main() {
     Intervention* intervention1 = maintenanceFactory.createIntervention();
     Intervention* intervention2 = urgenceFactory.createIntervention();
 
-    std::cout << "Intervention 1: " << intervention1->getType() << " - " << intervention1->getDetails() << std::endl;
-    std::cout << "Intervention 2: " << intervention2->getType() << " - " << intervention2->getDetails() << std::endl;
+    std::cout << "[Factory] Intervention 1 : " << intervention1->getType() 
+              << " - " << intervention1->getDetails() << std::endl;
 
+    std::cout << "[Factory] Intervention 2 : " << intervention2->getType() 
+              << " - " << intervention2->getDetails() << std::endl;
+
+    Intervention* intervention3 = new SuiviGPSDecorator(new Maintenance());
+    Intervention* intervention4 = new PiecesJointesDecorator(new Urgence());
+    Intervention* intervention5 = new PiecesJointesDecorator(new SuiviGPSDecorator(new Maintenance()));
+
+    std::cout << "\n[Decorator] Intervention 3 : " << intervention3->getType() 
+              << " - " << intervention3->getDetails() << std::endl;
+
+    std::cout << "[Decorator] Intervention 4 : " << intervention4->getType() 
+              << " - " << intervention4->getDetails() << std::endl;
+
+    std::cout << "[Decorator] Intervention 5 : " << intervention5->getType() 
+              << " - " << intervention5->getDetails() << std::endl;
+
+    // Nettoyage mémoire
     delete intervention1;
     delete intervention2;
+    delete intervention3;
+    delete intervention4;
+    delete intervention5;
 
     return 0;
 }

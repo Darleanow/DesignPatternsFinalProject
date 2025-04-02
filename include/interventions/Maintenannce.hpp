f24237882096cab09e1eabf7eacfdb570b975d3b
JørR <43743804+Ezyrone@users.noreply.github.com>
2025-04-02 15:43:30 +0200

    feat(Includes): Add Maintennannce hpp file

---
+++ include/interventions/Maintenannce.hpp
@@ -0,0 +1,12 @@
+#ifndef MAINTENANCE_HPP
+#define MAINTENANCE_HPP
+
+#include "Intervention.hpp"
+
+class Maintenance : public Intervention {
+public:
+    std::string getType() const override { return "Maintenance"; }
+    std::string getDetails() const override { return "Maintenance de l'appareil"; }
+};
+
+#endif

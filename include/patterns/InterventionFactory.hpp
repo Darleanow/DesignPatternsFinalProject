53d00cba89d6672b58aafe8da9e91c7d8769812b
JørR <43743804+Ezyrone@users.noreply.github.com>
2025-04-02 15:53:18 +0200

    feat(Includes): Add Factory pattern include file

---
+++ include/patterns/InterventionFactory.hpp
@@ -0,0 +1,12 @@
+#ifndef INTERVENTION_FACTORY_HPP
+#define INTERVENTION_FACTORY_HPP
+
+#include "../interventions/Intervention.hpp"
+
+class InterventionFactory {
+public:
+    virtual ~InterventionFactory() = default;
+    virtual Intervention* createIntervention() const = 0;
+};
+
+#endif

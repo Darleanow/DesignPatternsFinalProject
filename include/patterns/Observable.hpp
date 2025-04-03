df3b3af4672962df6a54cf9834a2c1874a3067bd
JørR <43743804+Ezyrone@users.noreply.github.com>
2025-04-03 08:38:17 +0200

    feat(Includes): Add Observable hpp file

---
+++ include/patterns/Observable.hpp
@@ -0,0 +1,23 @@
+#ifndef OBSERVABLE_HPP
+#define OBSERVABLE_HPP
+
+#include "IObserver.hpp"
+#include <vector>
+
+class Observable {
+    protected:
+    std::vector<IObserver*> observers;
+
+    public:
+    void addObserver(IObserver* observer) {
+        observers.push_back(observer);
+    }
+
+    void notifyObservers(const std::string& message) {
+        for (IObserver* observer : observers) {
+            observer->update(message);
+        }
+    }
+};
+
+#endif

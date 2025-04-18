dfd55b9778d51fe42f60404d97c81bf1c0bd6474
Enzo Hugonnier <enzohugonn@gmail.com>
2025-04-19 00:39:20 +0200

    feat(GUI): Setup GUI for technicians

--- include/TechMa/Views/Pages/TechniciansPage.h
+++ include/TechMa/Views/Pages/TechniciansPage.h
@@ -2,6 +2,9 @@
 
 #include <QWidget>
 
+class QPushButton;
+class CreateTechnicianDialog;
+
 class TechniciansPage : public QWidget
 {
   Q_OBJECT;
@@ -10,6 +13,9 @@ public:
   explicit TechniciansPage(QWidget *parent);
 
 private:
-  void setup_ui();
-  void setup_connects();
+  void                    setup_ui();
+  void                    setup_connects();
+
+  QPushButton            *m_create_button;
+  CreateTechnicianDialog *m_create_dialog;
 };
--- src/Views/Pages/TechniciansPage.cpp
+++ src/Views/Pages/TechniciansPage.cpp
@@ -1,6 +1,10 @@
 #include "TechMa/Views/Pages/TechniciansPage.h"
+#include "TechMa/Views/Widgets/CreateTechnicianDialog.h"
 
 #include <QLabel>
+#include <QPushButton>
+#include <qboxlayout.h>
+#include <qdialog.h>
 
 TechniciansPage::TechniciansPage(QWidget *parent) : QWidget(parent)
 {
@@ -10,8 +14,29 @@ TechniciansPage::TechniciansPage(QWidget *parent) : QWidget(parent)
 
 void TechniciansPage::setup_ui()
 {
+  auto  top_bar = new QHBoxLayout(this);
+
   auto *page_name = new QLabel("Technicians", this);
   page_name->setObjectName("Title");
+
+  m_create_button = new QPushButton("Create Technician", this);
+
+  top_bar->addWidget(page_name);
+  top_bar->addWidget(m_create_button);
+  setLayout(top_bar);
+
+  m_create_dialog = new CreateTechnicianDialog(this);
 }
 
-void TechniciansPage::setup_connects() {}
+void TechniciansPage::setup_connects()
+{
+  connect(m_create_button, &QPushButton::clicked, m_create_dialog, [this]() {
+    if(m_create_dialog->exec() == QDialog::Accepted) {
+      auto selected = m_create_dialog->selected_expertise();
+
+      for(auto field : selected) {
+        qDebug() << QString::fromStdString(to_string(field));
+      }
+    }
+  });
+}

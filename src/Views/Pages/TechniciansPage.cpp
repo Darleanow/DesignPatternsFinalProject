f4f1b728ffc421b51f9331ea38da7bb5e36592e4
Enzo Hugonnier <enzohugonn@gmail.com>
2025-04-19 20:36:33 +0200

    refactor(TechniciansPage): Rearrange UI setup and improve technician creation logic

--- src/Views/Pages/TechniciansPage.cpp
+++ src/Views/Pages/TechniciansPage.cpp
@@ -1,5 +1,9 @@
 #include "TechMa/Views/Pages/TechniciansPage.h"
+#include "TechMa/Repository/TechnicianRepository.h"
+#include "TechMa/Technician/BasicTechnician.h"
+#include "TechMa/Technician/ExpertiseDecorator.h"
 #include "TechMa/Views/Widgets/CreateTechnicianDialog.h"
+#include "TechMa/Views/Widgets/TechniciansListView.h"
 
 #include <QLabel>
 #include <QPushButton>
@@ -19,10 +23,15 @@ void TechniciansPage::setup_ui()
   auto *page_name = new QLabel("Technicians", this);
   page_name->setObjectName("Title");
 
+  top_bar->addWidget(page_name);
+
   m_create_button = new QPushButton("Create Technician", this);
 
-  top_bar->addWidget(page_name);
   top_bar->addWidget(m_create_button);
+
+  m_list_view = new TechniciansListView(this);
+  top_bar->addWidget(m_list_view);
+
   setLayout(top_bar);
 
   m_create_dialog = new CreateTechnicianDialog(this);
@@ -32,11 +41,22 @@ void TechniciansPage::setup_connects()
 {
   connect(m_create_button, &QPushButton::clicked, m_create_dialog, [this]() {
     if(m_create_dialog->exec() == QDialog::Accepted) {
-      auto selected = m_create_dialog->selected_expertise();
+      auto  full_name           = m_create_dialog->full_name();
+      auto  selected_expertises = m_create_dialog->selected_expertise();
 
-      for(auto field : selected) {
-        qDebug() << QString::fromStdString(to_string(field));
+      auto &technician_repository = TechnicianRepository::instance();
+
+      std::shared_ptr<ATechnician> technician =
+          std::make_shared<BasicTechnician>(
+              technician_repository.next_id(), full_name.toStdString()
+          );
+
+      for(const auto &expertise : selected_expertises) {
+        technician =
+            std::make_shared<ExpertiseDecorator>(technician, expertise);
       }
+
+      technician_repository.save(technician);
     }
   });
 }

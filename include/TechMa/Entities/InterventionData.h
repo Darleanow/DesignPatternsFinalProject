2f65d12a69b1571991ab464e9826c386686a9c99
Enzo Hugonnier <enzohugonn@gmail.com>
2025-04-21 01:45:34 +0200

    feat(InterventionData): Add InterventionData struct with relevant fields and types

---
+++ include/TechMa/Entities/InterventionData.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "TechMa/Entities/ATechnician.h"
+#include "TechMa/Entities/Location.h"
+#include "TechMa/Types/ExpertiseField.h"
+#include "TechMa/Types/InterventionComplexity.h"
+
+#include <chrono>
+#include <memory>
+#include <optional>
+#include <string>
+
+struct InterventionData
+{
+  int                                 id;
+  std::chrono::year_month_day         date;
+  std::chrono::hours                  anticipated_duration;
+  std::optional<std::chrono::minutes> actual_duration;
+  std::shared_ptr<ATechnician>        technician;
+  ExpertiseField                      expertise_required;
+  InterventionComplexity              complexity;
+  Location                            location;
+  std::string                         notes;
+  std::optional<int>                  technician_id;
+  bool                                completed = false;
+};

4b3a9944e1229a81054c50b9cd28c128e3020a62
Jaden38 <damien.nithard@gmail.com>
2025-04-22 10:27:05 +0200

    feat(TechMapp): Enhance UI with user status display and implement login/logout functionality

--- include/TechMa/Views/TechMapp.h
+++ include/TechMa/Views/TechMapp.h
@@ -1,12 +1,15 @@
 #pragma once
 
+#include "TechMa/Authentication/UserAuthentication.h"
 #include "TechMa/Views/Widgets/NavigationBar.h"
 
+#include <QLabel>
 #include <QMainWindow>
 
 class QApplication;
 class NavigationBar;
 class QStackedWidget;
+class LoginPage;
 
 class TechMapp : public QMainWindow
 {
@@ -15,12 +18,20 @@ class TechMapp : public QMainWindow
 public:
   explicit TechMapp(QApplication &app);
 
+private slots:
+  void on_login_successful(UserRole role);
+  void on_logout_requested();
+
 private:
   void            load_stylesheet();
   void            setup_font(QApplication &app);
   void            setup_ui();
   void            setup_connects();
+  void            update_ui_for_role(UserRole role);
 
   NavigationBar  *m_navigation_bar;
   QStackedWidget *m_pages;
+  LoginPage      *m_login_page;
+  QLabel         *m_user_status_label;
+  UserRole        m_current_role = UserRole::GUEST;
 };
--- src/Views/TechMapp.cpp
+++ src/Views/TechMapp.cpp
@@ -1,14 +1,19 @@
 #include "TechMa/Views/TechMapp.h"
+#include "TechMa/Authentication/UserAuthentication.h"
 #include "TechMa/Views/Pages/InterventionPage.h"
+#include "TechMa/Views/Pages/LoginPage.h"
 #include "TechMa/Views/Pages/ParametersPage.h"
 #include "TechMa/Views/Pages/TechniciansPage.h"
 
 #include <QApplication>
 #include <QFile>
 #include <QFontDatabase>
+#include <QHBoxLayout>
 #include <QLabel>
+#include <QMessageBox>
 #include <QPushButton>
 #include <QStackedWidget>
+#include <QStatusBar>
 #include <QVBoxLayout>
 
 TechMapp::TechMapp(QApplication &app)
@@ -17,6 +22,10 @@ TechMapp::TechMapp(QApplication &app)
   setup_font(app);
   setup_ui();
   setup_connects();
+
+  m_pages->setCurrentIndex(3);
+
+  update_ui_for_role(UserRole::GUEST);
 
   show();
 }
@@ -60,8 +69,20 @@ void TechMapp::setup_ui()
   vlayout->setContentsMargins(0, 0, 0, 0);
   vlayout->setSpacing(0);
 
+  auto *top_bar    = new QWidget(this);
+  auto *top_layout = new QHBoxLayout(top_bar);
+
   m_navigation_bar = new NavigationBar(this);
-  vlayout->addWidget(m_navigation_bar);
+  top_layout->addWidget(m_navigation_bar);
+
+  m_user_status_label = new QLabel("Guest", this);
+  m_user_status_label->setStyleSheet(
+      "color: #6B6BFF; font-weight: bold; padding-right: 10px;"
+  );
+  top_layout->addWidget(m_user_status_label, 0, Qt::AlignRight);
+
+  top_bar->setLayout(top_layout);
+  vlayout->addWidget(top_bar);
 
   m_pages = new QStackedWidget(this);
   vlayout->addWidget(m_pages);
@@ -69,14 +90,84 @@ void TechMapp::setup_ui()
   m_pages->addWidget(new InterventionPage(this));
   m_pages->addWidget(new TechniciansPage(this));
   m_pages->addWidget(new ParametersPage(this));
+  m_login_page = new LoginPage(this);
+  m_pages->addWidget(m_login_page);
 
   setCentralWidget(central);
+
+  auto statusBar = new QStatusBar(this);
+  auto logoutBtn = new QPushButton("Logout", statusBar);
+
+  connect(
+      logoutBtn, &QPushButton::clicked, this, &TechMapp::on_logout_requested
+  );
+
+  statusBar->addPermanentWidget(logoutBtn);
+  setStatusBar(statusBar);
 }
 
 void TechMapp::setup_connects()
 {
   connect(
-      m_navigation_bar, &NavigationBar::tabChanged, m_pages,
-      &QStackedWidget::setCurrentIndex
+      m_navigation_bar, &NavigationBar::tabChanged, this,
+      [this](int index) {
+        if(m_pages->currentIndex() != 3) {
+          m_pages->setCurrentIndex(index);
+        }
+      }
   );
+
+  connect(
+      m_login_page, &LoginPage::login_successful, this,
+      &TechMapp::on_login_successful
+  );
+  connect(
+      m_login_page, &LoginPage::logout_requested, this,
+      &TechMapp::on_logout_requested
+  );
+}
+
+void TechMapp::on_login_successful(UserRole role)
+{
+  m_current_role = role;
+  update_ui_for_role(role);
+
+  m_pages->setCurrentIndex(0);
+  m_navigation_bar->setCurrentIndex(0);
+}
+
+void TechMapp::on_logout_requested()
+{
+  AuthenticationService::instance().logout();
+
+  m_pages->setCurrentIndex(3);
+
+  update_ui_for_role(UserRole::GUEST);
+}
+
+void TechMapp::update_ui_for_role(UserRole role)
+{
+  QString role_name;
+  switch(role) {
+  case UserRole::ADMIN:
+    role_name = "Admin";
+    break;
+  case UserRole::TECHNICIAN:
+    role_name = "Technician";
+    break;
+  case UserRole::GUEST:
+  default:
+    role_name = "Guest";
+    break;
+  }
+
+  auto    user     = AuthenticationService::instance().current_user();
+  QString username = user ? QString::fromStdString(user->username()) : "";
+
+  if(!username.isEmpty()) {
+    m_user_status_label->setText(QString("%1 (%2)").arg(username).arg(role_name)
+    );
+  } else {
+    m_user_status_label->setText(role_name);
+  }
 }
